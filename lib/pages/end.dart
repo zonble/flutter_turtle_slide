0b37995b330ff58925f321543cde32298048c1c6
zonble <zonble@gmail.com>
2020-02-12 10:45:26 +0800

    Updates examples.

--- lib/pages/end.dart
+++ lib/pages/end.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_turtle/flutter_turtle.dart';
 
-class EndPage extends StatelessWidget {
+class EndPage extends StatefulWidget {
+  @override
+  _EndPageState createState() => _EndPageState();
+}
+
+class _EndPageState extends State<EndPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-            child: Text(
-      'Thanks!',
-      style: Theme.of(context).textTheme.display1,
-    )));
+    return GestureDetector(
+      onTap: () => setState(() {}),
+      child: AnimatedTurtleView(
+        animationDuration: Duration(milliseconds: 250),
+        child: Container(width: double.infinity, height: double.infinity),
+        commands: [
+          Right((_) => 90),
+          Repeat((_) => 18, [
+            Right((_) => 20),
+            Forward((_) => 10),
+            SetColor(
+                (_) => Colors.black.withOpacity(_['repcount'].toDouble() / 18)),
+            SetLabelHeight((_) => _['repcount'] * 2.0 + 20.0),
+            Label((_) => 'Thanks'),
+            Back((_) => 10),
+          ]),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/example2.dart
+++ lib/pages/example2.dart
@@ -15,28 +15,25 @@ class Example2Code extends StatelessWidget {
               Divider(),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text(''' 
+                child: Text('''
 AnimatedTurtleView(
   commands: [
     SetMacro('tree', [
-      IfElse((_) => _['l'] < 5.0, [
-      Stop()
+      IfElse((_) => _['l'] < 2.0, [
+        Stop()
       ], [
+        IfElse((_) => _['l'] < 10.0, [SetColor((_) => Colors.green)],
+            [SetColor((_) => Colors.brown)]),
         SetStrokeWidth((_) => _['l'] / 3.0),
         Forward((_) => _['l']),
-        Right((_) => 30.0),
-        Forward((_) => _['l'] / 2.0),
-        RunMacro('tree', (_) => {'l': _['l'] * 3.0 / 4.0}),
-        Back((_) => _['l'] / 2.0),
-        Left((_) => 60),
-        Forward((_) => _['l'] / 2.0),
-        RunMacro('tree', (_) => {'l': _['l'] * 3.0 / 4.0}),
-        Back((_) => _['l'] / 2.0),
+        Right((_) => 30.0), RunMacro('tree', (_) => {'l': _['l'] * 3.0 / 4.0}),
+        Left((_) => 60), RunMacro('tree', (_) => {'l': _['l'] * 3.0 / 4.0}),
       ]),
     ]),
-    Back((_) => 120),
     PenDown(),
-    RunMacro('tree', (_) => {'l':60.0})
+    Back((_) => 120),
+    SetColor((_) => Colors.brown),
+    RunMacro('tree', (_) => {'l': 100.0})
  ],
 )
                 ''', style: TextStyle(fontFamily: 'Courier')),
@@ -59,33 +56,35 @@ class _Example2State extends State<Example2> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => setState(() {}),
-      child: AnimatedTurtleView(
-        commands: [
-          SetMacro('tree', [
-            IfElse((_) => _['l'] < 2.0, [
-              Stop()
-            ], [
-              IfElse((_) => _['l'] < 10.0, [SetColor((_) => Colors.green)],
-                  [SetColor((_) => Colors.brown)]),
-              SetStrokeWidth((_) => _['l'] / 3.0),
+      child: ClipRect(
+        child: Center(
+          child: AnimatedTurtleView(
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            commands: [
+              SetMacro('tree', [
+                IfElse((_) => _['l'] < 2.0, [
+                  Stop()
+                ], [
+                  IfElse((_) => _['l'] < 10.0, [SetColor((_) => Colors.green)],
+                      [SetColor((_) => Colors.brown)]),
+                  SetStrokeWidth((_) => _['l'] / 3.0),
+                  Forward((_) => _['l']),
+                  Right((_) => 30.0),
+                  RunMacro('tree', (_) => {'l': _['l'] * 3.0 / 4.0}),
+                  Left((_) => 60),
+                  RunMacro('tree', (_) => {'l': _['l'] * 3.0 / 4.0}),
+                ]),
+              ]),
               PenDown(),
-              Forward((_) => _['l']),
-              Right((_) => 30.0),
-              Forward((_) => _['l'] / 2.0),
-              RunMacro('tree', (_) => {'l': _['l'] * 3.0 / 4.0}),
-              PenUp(),
-              Back((_) => _['l'] / 2.0),
-              PenDown(),
-              Left((_) => 60),
-              Forward((_) => _['l'] / 2.0),
-              RunMacro('tree', (_) => {'l': _['l'] * 3.0 / 4.0}),
-              Back((_) => _['l'] / 2.0),
-            ]),
-          ]),
-          Back((_) => 120),
-          SetColor((_) => Colors.brown),
-          RunMacro('tree', (_) => {'l': 60.0})
-        ],
+              Back((_) => 120),
+              SetColor((_) => Colors.brown),
+              RunMacro('tree', (_) => {'l': 100.0})
+            ],
+          ),
+        ),
       ),
     );
   }
