9b4ee4ba9a024ade3daf5e689d001fab13d66d1f
zonble <zonble@gmail.com>
2020-03-12 02:05:12 +0800

    Updates the examples.

--- lib/pages/lsystem.dart
+++ lib/pages/lsystem.dart
@@ -9,67 +9,87 @@ class LSystemPage extends StatefulWidget {
   _LSystemPageState createState() => _LSystemPageState();
 }
 
-class _LSystemPageState extends State<LSystemPage> {
+class _LSystemPageState extends State<LSystemPage>
+    with SingleTickerProviderStateMixin {
   double _value = 10.0;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 10));
+    controller.repeat(reverse: true, period: Duration(seconds: 5));
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          TurtleView(
-            commands: [
-              SetMacro('a', [
-                IfElse((_) => _['l'] < 10.0, [
-                  Stop()
-                ], [
-                  IfElse((_) => _['l'] < 30.0, [
-                    SetColor((_) => Colors.green),
-                    SetStrokeWidth((_) => 8),
-                  ], [
-                    SetColor((_) => Colors.brown),
-                    SetStrokeWidth((_) => min(_['l'] / 5.0, 50.0)),
-                  ]),
-                  Back((_) => 4.0),
-                  Forward((_) => 4.0),
-                  Forward((_) => min(_['l'], 60.0)),
-                  Left((_) => 20.0),
-                  RunMacro('a', (_) => {'l': (_['l'] / 5.0 * 4.0)}),
-                  Right((_) => 40.0),
-                  RunMacro('b', (_) => {'l': (_['l'] / 5.0 * 4.0)}),
-                ]),
-              ]),
-              SetMacro('b', [
-                IfElse((_) => _['l'] < 10.0, [
-                  Stop()
-                ], [
-                  IfElse((_) => _['l'] < 30.0, [
-                    SetColor((_) => Colors.green),
-                    SetStrokeWidth((_) => 8),
-                  ], [
-                    SetColor((_) => Colors.brown),
-                    SetStrokeWidth((_) => min(_['l'] / 5.0, 50.0)),
-                  ]),
-                  Back((_) => 4.0),
-                  Forward((_) => 4.0),
-                  Forward((_) => min(_['l'], 60.0)),
-                  Right((_) => 20.0),
-                  RunMacro('a', (_) => {'l': (_['l'] / 5.0 * 4.0)}),
-                ]),
-              ]),
-              Back((_) => 200),
-              PenDown(),
-              RunMacro('a', (_) => {'l': _value}),
-            ],
-            child: Container(width: double.infinity, height: 400),
-          ),
+          AnimatedBuilder(
+              animation: controller,
+              builder: (context, snapshot) {
+                return TurtleView(
+                  commands: [
+                    SetMacro('a', [
+                      IfElse((_) => _['l'] < 10.0, [
+                        Stop()
+                      ], [
+                        IfElse((_) => _['l'] < 30.0, [
+                          SetColor((_) => Colors.green),
+                          SetStrokeWidth((_) => 8),
+                        ], [
+                          SetColor((_) => Colors.brown),
+                          SetStrokeWidth((_) => min(_['l'] / 5.0, 50.0)),
+                        ]),
+                        Back((_) => 4.0),
+                        Forward((_) => 4.0),
+                        Forward((_) => min(_['l'], 60.0)),
+                        Left((_) => 20.0 + (controller.value * 5)),
+                        RunMacro('a', (_) => {'l': (_['l'] / 5.0 * 4.0)}),
+                        Right((_) => 40.0 - (controller.value * 5)),
+                        RunMacro('b', (_) => {'l': (_['l'] / 5.0 * 4.0)}),
+                      ]),
+                    ]),
+                    SetMacro('b', [
+                      IfElse((_) => _['l'] < 10.0, [
+                        Stop()
+                      ], [
+                        IfElse((_) => _['l'] < 30.0, [
+                          SetColor((_) => Colors.green),
+                          SetStrokeWidth((_) => 8),
+                        ], [
+                          SetColor((_) => Colors.brown),
+                          SetStrokeWidth((_) => min(_['l'] / 5.0, 50.0)),
+                        ]),
+                        Back((_) => 4.0),
+                        Forward((_) => 4.0),
+                        Forward((_) => min(_['l'], 60.0)),
+                        Right((_) => 20.0 + (controller.value * 10)),
+                        RunMacro('a', (_) => {'l': (_['l'] / 5.0 * 4.0)}),
+                      ]),
+                    ]),
+                    Back((_) => 200),
+                    PenDown(),
+                    RunMacro('a', (_) => {'l': _value}),
+                  ],
+                  child: Container(width: double.infinity, height: 400),
+                );
+              }),
           SizedBox(height: 20),
           Container(
             width: 300,
             child: Slider(
               min: 10.0,
-              max: 300.0,
+              max: 250.0,
               value: _value,
               onChanged: (value) => setState(() => _value = value),
             ),
